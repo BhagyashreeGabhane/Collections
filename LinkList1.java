766a3641a8654d28f0fc8dcbb77c65066a78d050
BhagyashreeGabhane <86108291+BhagyashreeGabhane@users.noreply.github.com>
2022-08-20 22:18:31 +0530

    Add files via upload

---
+++ LinkList1.java
@@ -0,0 +1,20 @@
+package listInterface;
+
+import java.util.LinkedList;
+
+public class LinkList1 {
+
+	public static void main(String[] args) {
+		LinkedList<Integer> ll=new LinkedList<Integer>();
+		ll.add(4);
+		ll.add(3);
+		ll.add(5);
+		ll.add(2);
+		ll.add(87);
+		ll.add(23);
+		ll.add(4);
+		
+		System.out.println(ll);
+	}
+
+}
