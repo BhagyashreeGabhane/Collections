bdfd274e94f3612cc412ec0f521d4a921315805e
BhagyashreeGabhane <86108291+BhagyashreeGabhane@users.noreply.github.com>
2022-08-20 22:17:05 +0530

    Add files via upload

---
+++ compairdisc.java
@@ -0,0 +1,18 @@
+package MapsDis;
+
+import java.util.Comparator;
+
+public class compairdisc implements Comparator<Integer> {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int compare(Integer o1, Integer o2) {
+		// TODO Auto-generated method stub
+		return o1.compareTo(o2);
+	}
+
+}
